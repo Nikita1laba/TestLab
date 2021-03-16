b60baf541a48b8488fcf031b9ce087a3af136442
Куц Никита Дмитриевич <318213@college.local>
2021-03-16 12:25:27 +0300

    add Class tovar

---
+++ tovar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPLab1
+{
+    public class tovar
+    {
+        public string Name { get; set; }
+        public int kol { get; set; }
+        public float stoim { get; set; }
+        public tovar (string Name,int kol,float stoim)
+        {
+            this.Name = Name;
+            this.kol = kol;
+            this.stoim = stoim;
+        }
+    }
+}
