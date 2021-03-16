8f8b2172a2eebfbe19eba727929f6369d5655e4e
Куц Никита Дмитриевич <318213@college.local>
2021-03-16 12:21:41 +0300

    add Class program

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<tovar> tovarList = new List<tovar>();
+            while (true)
+            
+            {  
+                {
+                    Console.WriteLine("Введите имя: ");
+                    string Name = Console.ReadLine();
+                    Console.WriteLine("Введите Количество: ");
+                    int kol = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите стоимость: ");
+                    float stoim = Convert.ToInt32(Console.ReadLine());
+                    tovarList.Add(new tovar(Name, kol, stoim));
+                    Console.Clear();
+              
+                    for(int i = 0; i < tovarList.Count; i++)
+                    {
+                        Console.WriteLine("Имя: " + tovarList[i].Name + "Количество : " + tovarList[i].kol + "стоимость: " + tovarList[i].stoim);
+                    }
+                }
+            }
+        }
+    }
+}
